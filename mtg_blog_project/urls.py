cd8ddb1c147154f9babeb3c7d5f0f76ae1440f4c
sheridan-jeremy-t <tempestj@sheridancollege.ca>
2025-06-02 08:05:51 -0400

    created index view

--- mtg_blog_project/urls.py
+++ mtg_blog_project/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from index import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index', views.index)
 ]
